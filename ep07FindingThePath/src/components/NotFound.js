a380156e46c329f62f67598e7d6068c5ed821f56
Gururaj Chadaga <guruuraj.chadaga@gmail.com>
2023-10-24 02:44:59 +0530

    ep07

--- ep07FindingThePath/src/components/NotFound.js
+++ ep07FindingThePath/src/components/NotFound.js
@@ -2,10 +2,6 @@ import { useRouteError } from 'react-router-dom';
 
 const NotFound = () => {
   const error = useRouteError();
-  return (
-    <div>
-      <h1>{`${error.status}: ${error.statusText}`}</h1>
-    </div>
-  );
+  return <div>{`${error.status}: ${error.statusText}`}</div>;
 };
 export default NotFound;
